3cb6c14a293d163a0af291a8079149f79aa7b7f0
clentebanks <clentebanks0@gmail.com>
2023-03-24 22:10:57 -0600

    errfr

---
+++ CapaNegocio/CN_Recursos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    public class CN_Recursos
+    {
+        //encriptacio del TEXTO en SHA256
+
+        public static string ConvertirSha256(string texto)
+        {
+            StringBuilder sb = new StringBuilder();
+            //usar la referencia del "sytem.security.cryptography"
+            using(SHA256 hash = SHA256Managed.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                byte[] result = hash.ComputeHash(enc.GetBytes(texto));
+
+                foreach(byte b in result)
+       
+                    sb.Append(b.ToString("x2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
